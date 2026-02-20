12d9386ac496a12ad31b3b623e31283502cbfd6e
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2026-02-20 20:31:33 +0000

    chore: comment updated

--- test/fixtures/handleCommand/HandleCommandTestFixture.sol
+++ test/fixtures/handleCommand/HandleCommandTestFixture.sol
@@ -99,7 +99,7 @@ library HandleCommandTestFixture {
         // 1) Read the raw bytes
         bytes memory publicInputsFieldsData = vm.readFileBinary(path);
 
-        // 2) Decode the blob into fixed bytes32[154]
+        // 2) Decode the blob into fixed bytes32[155]
         (bytes32[155] memory publicInputsFixed) = abi.decode(publicInputsFieldsData, (bytes32[155]));
 
         // 3) Convert to dynamic bytes32[]
